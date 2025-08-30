2c57f0065d3e0773c8ca309c56c52ca5613a13c8
Ubuntu <ubuntu@ip-172-31-82-140.ec2.internal>
2025-08-30 09:40:23 +0000

    EC2 Instance commit

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_instance" "first_app_server" {
+resource "aws_instance" "first_server" {
   ami           = "ami-0cfde0ea8edd312d4"
   instance_type = "t2.micro"
 
