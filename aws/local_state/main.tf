492954d6ce10daa1b9f2bef114a0cd7d1598320c
Ubuntu <ubuntu@ip-172-31-82-140.ec2.internal>
2025-08-30 09:34:36 +0000

    EC2 Commit

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.13.1"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "first_app_server" {
+  ami           = "ami-0cfde0ea8edd312d4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
 
