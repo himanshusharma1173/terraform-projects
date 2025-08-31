9ea4be47935a397f64475d9d0d6a5dee135c27ab
Himanshu Sharma <himanshusharma110703@gmail.com>
2025-08-31 18:18:07 +0530

    Update output.tf

--- aws/remote_state/output.tf
+++ aws/remote_state/output.tf
@@ -1,7 +1,5 @@
-# outputs.tf
-# ---------------------------
 # Useful outputs after deployment
-# ---------------------------
+
 
 output "s3_bucket_name" {
   value       = aws_s3_bucket.terraform_state.id
