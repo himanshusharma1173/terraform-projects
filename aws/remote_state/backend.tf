b893a8bc0875716672ca6f96821f2f03f3638de8
Himanshu Sharma <himanshusharma110703@gmail.com>
2025-08-31 18:15:44 +0530

    Update backend.tf

--- aws/remote_state/backend.tf
+++ aws/remote_state/backend.tf
@@ -1,8 +1,4 @@
-# backend.tf
-# ---------------------------
-# Define backend type (S3 + DynamoDB)
-# Actual values are passed via -backend-config
-# ---------------------------
+# Define backend type 
 
 terraform {
   backend "s3" {}
