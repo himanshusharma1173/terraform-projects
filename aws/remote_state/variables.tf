aaeca1a6e404adfd2f2b7975f8954d6b85547938
Himanshu Sharma <himanshusharma110703@gmail.com>
2025-08-31 18:19:47 +0530

    Update variables.tf

--- aws/remote_state/variables.tf
+++ aws/remote_state/variables.tf
@@ -1,7 +1,5 @@
-# variables.tf
-# ---------------------------
 # Input variables used across the project
-# ---------------------------
+
 
 variable "aws_region" {
   description = "The AWS region where resources will be created"
