6e62c37b1ae76e6f9de38769d494bfce09b21c91
Himanshu Sharma <himanshusharma110703@gmail.com>
2025-08-31 18:18:47 +0530

    Update provider.tf

--- aws/remote_state/provider.tf
+++ aws/remote_state/provider.tf
@@ -1,8 +1,6 @@
-# provider.tf
-# ---------------------------
 # AWS Provider configuration
 # Region is passed via variable (from variables.tf or terraform.tfvars)
-# ---------------------------
+
 
 provider "aws" {
   region = var.aws_region
